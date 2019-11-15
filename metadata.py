7bf3f237b0652e5be262c9d6d5d6252efd878cc7
ademir <andacdemir@hotmail.com>
2019-11-15 18:01:13 -0500

    using benchmark datasets

--- metadata.py
+++ metadata.py
@@ -24,44 +24,121 @@ FC100:
 Based on CIFAR-100 with the objective to minimize the information overlap 
 between class splits. 32x32 color images belonging to 100 different classes
 are further grouped into 20 superclasses. We split the dataset by
-superclass, rather than by individual class to minimize the information overlap. 
+superclass, rather than by individual class to minimize the information overlap 
 Thus the train split contains 60 classes belonging to 12 superclasses, 
 the validation and test contain 20 classes belonging to 5 superclasses each.
+
+Each dataset D_i in the meta-train, meta-validation and meta-test data 
+is separated in two parts: a training set (with N unique data) to adapt  
+the model to the task at hand, and a test set (or query set) 
+for evaluation and meta-optimization. 
+No same image can be both in the training and test set. 
 '''
 
 from torchmeta.datasets import MiniImagenet, CIFARFS, FC100, TieredImagenet
 from torchmeta.transforms import Categorical, ClassSplitter, Rotation
+from torchvision.transforms import Compose, Resize, ToTensor
 from torchmeta.utils.data import BatchMetaDataLoader
-import torchvision.transforms as transforms
 
-class Metadata():
-    def __init__(self, K, )
-dataset = Omniglot("data",
-                   # Number of ways
-                   num_classes_per_task=5,
-                   # Resize the images to 28x28 and converts them to PyTorch tensors (from Torchvision)
-                   transform=Compose([Resize(28), ToTensor()]),
-                   # Transform the labels to integers (e.g. ("Glagolitic/character01", "Sanskrit/character14", ...) to (0, 1, ...))
-                   target_transform=Categorical(num_classes=5),
-                   # Creates new virtual classes with rotated versions of the images (from Santoro et al., 2016)
-                   class_augmentations=[Rotation([90, 180, 270])],
-                   meta_train=True,
-                   download=True)
-                   
-dataset = ClassSplitter(dataset, shuffle=True, num_train_per_class=5, num_test_per_class=15)
-dataloader = BatchMetaDataLoader(dataset, batch_size=16, num_workers=4)
+class Metadata_generator():
+    def __init__(self, K, N, num_test_per_class, batch_size, dataset):
+        '''
+        K-shot N-way learning, each N from different unseen classes 
+        '''
+        self.K = K
+        self.N = N
+        self.num_test_per_class = num_test_per_class
+        self.batch_size = batch_size
+        self.dataset = dataset
+
+    def generate_batch(self):
+        '''
+        The data-loaders of torch meta are fully compatible with standard data
+        components of PyTorch, such as Dataset and DataLoader.
+        Augments the pool of class candidates with variants, such as rotated images
+        ''' 
+        if self.dataset == "miniImageNet":
+            dataset = MiniImagenet("data",
+                            # Number of ways
+                            num_classes_per_task=self.N,
+                            # Resize the images and converts them 
+                            # to PyTorch tensors (from Torchvision)
+                            transform=Compose([Resize(84), ToTensor()]),
+                            # Transform the labels to integers 
+                            target_transform=Categorical(num_classes=self.N),
+                            # Creates new virtual classes with rotated versions
+                            # of the images (from Santoro et al., 2016)
+                            class_augmentations=[Rotation([90, 180, 270])],
+                            meta_train=True,
+                            download=True)
+
+        if self.dataset == "CIFARFS":
+            dataset = CIFARFS("data",
+                            # Number of ways
+                            num_classes_per_task=self.N,
+                            # Resize the images and converts them 
+                            # to PyTorch tensors (from Torchvision)
+                            transform=Compose([Resize(84), ToTensor()]),
+                            # Transform the labels to integers 
+                            target_transform=Categorical(num_classes=self.N),
+                            # Creates new virtual classes with rotated versions
+                            # of the images (from Santoro et al., 2016)
+                            class_augmentations=[Rotation([90, 180, 270])],
+                            meta_train=True,
+                            download=True)
+
+        if self.dataset == "FC100":
+            dataset = FC100("data",
+                            # Number of ways
+                            num_classes_per_task=self.N,
+                            # Resize the images and converts them 
+                            # to PyTorch tensors (from Torchvision)
+                            transform=Compose([Resize(32), ToTensor()]),
+                            # Transform the labels to integers 
+                            target_transform=Categorical(num_classes=self.N),
+                            # Creates new virtual classes with rotated versions
+                            # of the images (from Santoro et al., 2016)
+                            class_augmentations=[Rotation([90, 180, 270])],
+                            meta_train=True,
+                            download=True)
+
+        if self.dataset == "tieredImageNet":
+            dataset = TieredImagenet("data",
+                            # Number of ways
+                            num_classes_per_task=self.N,
+                            # Resize the images and converts them 
+                            # to PyTorch tensors (from Torchvision)
+                            transform=Compose([Resize(32), ToTensor()]),
+                            # Transform the labels to integers 
+                            target_transform=Categorical(num_classes=self.N),
+                            # Creates new virtual classes with rotated versions
+                            # of the images (from Santoro et al., 2016)
+                            class_augmentations=[Rotation([90, 180, 270])],
+                            meta_train=True,
+                            download=True)        
+                        
+        dataset = ClassSplitter(dataset, shuffle=True, num_train_per_class=self.K, 
+                                num_test_per_class=self.num_test_per_class)
+
+        dataloader = BatchMetaDataLoader(self.dataset, batch_size=self.batch_size,
+                                         num_workers=2)
+        return dataloader
 
 def main():
-    dataloader = load_data(dataset)
-
+    dataset = "miniImageNet"
+    # dataset = "CIFARFS"
+    # dataset = "FC100"
+    # dataset = "tieredImageNet"
+    data_generator = Metadata_generator(5, 5, 15, 16, dataset)
+    dataloader = data_generator.generate_batch()
     for batch in dataloader:
         train_inputs, train_targets = batch["train"]
-        print('Train inputs shape: {0}'.format(train_inputs.shape))    # (16, 25, 1, 28, 28)
-        print('Train targets shape: {0}'.format(train_targets.shape))  # (16, 25)
+        print('Train inputs shape: {0}'.format(train_inputs.shape))    
+        print('Train targets shape: {0}'.format(train_targets.shape))  
 
         test_inputs, test_targets = batch["test"]
-        print('Test inputs shape: {0}'.format(test_inputs.shape))      # (16, 75, 1, 28, 28)
-        print('Test targets shape: {0}'.format(test_targets.shape))    # (16, 75) 
+        print('Test inputs shape: {0}'.format(test_inputs.shape))      
+        print('Test targets shape: {0}'.format(test_targets.shape))    
 
 
 if __name__ == '__main__':
